b28988a8b62681ae4c55e1cc13e1bbd59dbcf80f
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-08-19 22:51:38 -0700

    Add utility classes and examples

---
+++ triangle-net-debug/Assets/Contour.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class Contour : MonoBehaviour {
+
+    public bool hole = false;
+
+    public TriangleNet.Geometry.Contour GetContour()
+    {
+        return new TriangleNet.Geometry.Contour(GetPoints());
+    }
+
+    public TriangleNet.Geometry.Vertex[] GetPoints()
+    {
+        var v = GetVectors();
+        var p = new List<TriangleNet.Geometry.Vertex>();
+
+        for (int i = 0; i < v.Length; i++)
+        {
+            Vector3 vec = v[i];
+            p.Add(new TriangleNet.Geometry.Vertex(vec.x, vec.z));
+        }
+
+        return p.ToArray();
+    }
+
+    public Vector3[] GetVectors()
+    {
+        var trans = GetComponentsInChildren<Transform>();
+        var v = new List<Vector3>();
+
+        for(int i = 0; i < trans.Length; i ++)
+        {
+            if (trans[i] == transform) continue;
+            Vector3 vec = trans[i].localPosition;
+            vec.y = 0;
+            v.Add(vec);
+        }
+
+        return v.ToArray();
+    }
+
+    private void Update()
+    {
+        var trans = GetComponentsInChildren<Transform>();
+        foreach (var t in trans)
+        {
+            Vector3 v = t.localPosition;
+            v.y = 0;
+            t.localPosition = v;
+        }
+        
+
+
+        transform.position = Vector3.zero;
+        transform.rotation = Quaternion.identity;
+        transform.localScale = Vector3.one;
+
+        if (UnityEditor.Selection.Contains(gameObject.GetInstanceID()) && UnityEditor.Selection.gameObjects.Length > 1) {
+            List<GameObject> selection = new List<GameObject>(UnityEditor.Selection.gameObjects);
+            selection.Remove(gameObject);
+
+            UnityEditor.Selection.objects = selection.ToArray();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = hole ? Color.red : Color.white;
+        var vec = GetVectors();
+        for(int i = 0; i < vec.Length; i++)
+        {
+            int nexti = (i + 1) % vec.Length;
+
+            Vector3 thisvec = vec[i];
+            Vector3 nextvec = vec[nexti];
+
+            Gizmos.DrawLine(thisvec, nextvec);
+        }
+    }
+}
---
+++ triangle-net-debug/Assets/CreateMesh.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using TriangleNet.Geometry;
+using TriangleNet.Topology;
+public class CreateMesh : MonoBehaviour {
+
+    Mesh mesh = null;
+
+	// Update is called once per frame
+	void Update() {
+        var poly = new Polygon();
+        var cont = FindObjectsOfType<Contour>();
+		foreach(var c in cont)
+        {
+            poly.Add(c.GetContour(), c.hole);
+        }
+
+        var m = poly.Triangulate();
+        var verts = ToVector3(m.Vertices);
+        var tris = ToTriangles(m.Triangles);
+
+        if (mesh == null)
+        {
+            mesh = new Mesh();
+            mesh.MarkDynamic();
+            GetComponent<MeshFilter>().sharedMesh = mesh;
+        }
+
+        mesh.vertices = verts;
+        mesh.triangles = tris;
+    }
+
+    Vector3[] ToVector3(ICollection<Vertex> vert)
+    {
+        List<Vertex> vertList = new List<Vertex>();
+        foreach (var vt in vert) vertList.Add(vt);
+
+        var v = new Vector3[vert.Count];
+        for(int i = 0; i < vertList.Count; i ++)
+        {
+            v[i].x = (float)vertList[i].x;
+            v[i].z = (float)vertList[i].y;
+        }
+
+        return v;
+    }
+
+    int[] ToTriangles(ICollection<Triangle> tris)
+    {
+        List<Triangle> triList = new List<Triangle>();
+        foreach (var t in tris) triList.Add(t);
+
+
+        int[] ts = new int[tris.Count * 3];
+        for(int i = 0; i < triList.Count; i++)
+        {
+            Triangle tri = triList[i];
+            ts[i * 3 + 2] = tri.vertices[0].ID;
+            ts[i * 3 + 1] = tri.vertices[1].ID;
+            ts[i * 3 + 0] = tri.vertices[2].ID;
+        }
+
+        return ts;
+    }
+}
